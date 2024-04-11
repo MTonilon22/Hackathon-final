0fc63cb02df30e0da42f7c432abf94f461fa642c
MTonilon22 <mtonilon17@gmail.com>
2024-04-11 14:48:04 +0800

    gitarong ang register

--- src/MainPages/RegisterPage.vue
+++ src/MainPages/RegisterPage.vue
@@ -1,38 +1,264 @@
 <template>
-  <div class="flex flex-col items-center justify-center "> <p class="text-md font-bold tracking-wider">First Name</p>
-  <div class="">  <input class="rounded-lg border-[#2a2d57] " v-model="first_name" type="text">
+
+<div class="h-screen">
+    <div class="items-center justify-center flex cs:flex cs:flex-col">
+      <div
+        class="items-center justify-center mx-auto flex text-[#2a2d57] font-semibold tracking-widest"
+      ></div>
+      <div
+        class="lg:w-[100%] flex-col mx-auto cs:w-[100%] items-center justify-center flex lg:mt-[3%] cs:mt-[10%]"
+      >
+        <div class="w-[100%] rounded-2xl sm:max-w-md xl:p-0">
+          <div class="cs:px-5 lg:p-3 space-y-5 cs:w-[100%]">
+            <h1
+              class="w-[100%] font-semibold leading-tight tracking-wide cs:text-2xl lg:text-3xl text-[#2a2d57] md:text-2xl my-3"
+            >
+              <span class="text-ternary tracking-wide">Registration</span>
+              
+            </h1>
+            <p1 class="text-[#2a2d57] cs:text-sm tracking-wide"
+              >To register, please enter credentials.</p1>
+            <div class="space-y-5 lg:w-[100%]" action="#">
+              <div
+                class="lg:flex-row cs:flex-col items-center justify-center lg:flex gap-5 w-[100%] cs:space-y-5"
+              >
+                <div class="w-[100%]">
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                    >First Name</label
+                  >
+                  <input
+                    v-model="first_name"
+                    type="text"
+                    
+                   
+                    class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-primary-600 focus:border-primary-600 block w-[100%] p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Michael Tonilon"
+                    required=""
+                  />
+                </div>
+                <div class="w-[100%]">
+                  <label
+                    
+                    class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                    >Middle Name</label
+                  >
+                  <input
+                    v-model="middle_name"
+                    type="text"
+                 
+                    
+                    class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-primary-600 focus:border-primary-600 block w-[100%] p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="99999"
+                    required=""
+                  />
+                </div>
+                <div class="w-[100%]">
+                  <label
+                    
+                    class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                    >Last Name</label
+                  >
+                  <input
+                    v-model="last_name"
+                    type="text"
+                 
+                    
+                    class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-primary-600 focus:border-primary-600 block w-[100%] p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="99999"
+                    required=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Email</label
+              >
+              <input
+                v-model="email"
+                type="text"
+               
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="mtonilon@gmail.com"
+                required=""
+              />
+            </div>
+
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Birth Date</label
+              >
+              <input
+                v-model="birthdate"
+                type="date"
+              
+                placeholder=""
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Gender</label
+              >
+              <input
+                v-model="gender"
+                type="text"
+                
+                placeholder="Male"
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >School Graduated</label
+              >
+              <input
+                v-model="school_grad"
+                type="text"
+                
+                placeholder="University of Cebu"
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Experience</label
+              >
+              <input
+                v-model="experience"
+                type="text"
+                
+                placeholder="University of Cebu"
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Username</label
+              >
+              <input
+                v-model="username"
+                type="text"
+                
+                placeholder="Mtonilon22"
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Password</label
+              >
+              <input
+                v-model="password"
+                type="password"
+                
+                placeholder="******"
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Skills</label
+              >
+              <input
+                v-model="skills"
+                type="text"
+                
+                placeholder=""
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            
+          
+           
+           
+           
+            
+            
+            <div class="">
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Objective</label
+              >
+              <input
+                v-model="objective"
+                type="text"
+                
+                placeholder=""
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Position</label
+              >
+              <input
+                v-model="position"
+                type="text"
+                
+                placeholder=""
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                
+                class="block mb-2 text-sm font-medium text-[#2a2d57]"
+                >Achievement</label
+              >
+              <input
+                v-model="achievement"
+                type="text"
+                
+                placeholder=""
+                class="bg-white border font-500 border-gray-300 text-[#2a2d57] sm:text-sm rounded-md focus:ring-ternary focus:border-ternary block w-full p-2.5 dark:placeholder-gray-400 dark:text-[#2a2d57] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <input  type="file" @change="handleFileImage">
+    <button class="bg-[#2a2d57] px-5 py-2 my-5 rounded-lg text-white font-semibold text-sm" @click="register">SUBMIT</button>
+
+            <div class="flex gap-3 cs:w-[100%] cs:mx-auto">
+              
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   
-  <div> <p class="text-md font-bold tracking-wider">Middle Name</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="middle_name" type="text"></div> 
-  <div><p class="text-md font-bold tracking-wider">Last Name</p>
-  <div><input class="rounded-lg border-[#2a2d57] " v-model="last_name" type="text"></div>
-  <div><p class="text-md font-bold tracking-wider">Email</p></div>
-  <input class="rounded-lg border-[#2a2d57] " v-model="email" type="text"></div></div>
-  
-  <p class="text-md font-bold tracking-wider">Birth Date</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="birthdate" type="date">
-  <p class="text-md font-bold tracking-wider">Gender</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="gender" type="text">
-  <p class="text-md font-bold tracking-wider">School Graduated</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="school_grad" type="text">
-  <p class="text-md font-bold tracking-wider">Experience</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="experience" type="text">
-  <p class="text-md font-bold tracking-wider">Username</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="username" type="text">
-  <p class="text-md font-bold tracking-wider">Password</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="password" type="text">
-  <p class="text-md font-bold tracking-wider">Skills</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="skills" type="text">
-  <p class="text-md font-bold tracking-wider">Objective</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="objective" type="text">
-  <p class="text-md font-bold tracking-wider">Position</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="position" type="text">
-  <p class="text-md font-bold tracking-wider">Achievements</p>
-  <input class="rounded-lg border-[#2a2d57] " v-model="achievements" type="text">
-  <p class="text-md font-bold tracking-wider">Picture</p>
-  <input type="file" @change="handleFileImage">
-  <div class="my-5"><button class="px-5 py-2 rounded-lg bg-[#2a2d57] text-white my-2" @click="register">SUBMIT</button></div>
-  </div>
+ 
   
   </template>
   <script setup>
